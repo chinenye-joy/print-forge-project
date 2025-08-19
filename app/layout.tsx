41ad9354cd1cb46b4b6b2ef3f4a5e7fdf3a9c72d
chinenye-joy <anyimjoy01@gmail.com>
2025-08-19 23:56:14 +0100

    Enhance About page layout and content, add responsive navigation, and update global styles

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,20 @@
 import "./globals.css";
+import Link from "next/link";
+import { Albert_Sans, Montserrat_Alternates }from "next/font/google";
+import Image from "next/image";
+
+
+const albertSans =  Albert_Sans({
+  subsets: ["latin"],
+  display: "swap",
+});
+
+const montserratAlternates = Montserrat_Alternates({
+  subsets: ["latin"],
+  display: "swap",
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+  variable: "--font-montserrat-alternates",
+})
 
 export default function RootLayout({
   children,
@@ -7,7 +23,40 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>{children}</body>
+       <body className={`${albertSans.className} ${montserratAlternates.variable}`}>
+       <header className="w-full bg-white">
+          <nav className="flex justify-between px-6 py-4">
+          <Link href="/" >
+            <div className="relative">
+              {/* Desktop logo */}
+              <Image
+                src="/printforge-logo 1.png"
+                alt="PrintForge Logo"
+                width={200}
+                height={200}
+                className="w-[200px] h-auto hidden md:block"
+              />
+              {/* Mobile logo */}
+              <Image
+                src="/printforge-logo mobile.png"
+                alt="PrintForge Logo"
+                width={40}
+                height={40}
+                className="w-[40px] h-auto block md:hidden"
+              />
+            </div>
+          </Link>
+            <ul className="flex items-center gap-2.5">
+             <li className="text-sm uppercase cursor-pointer"><Link href="/3D models">3d-models</Link></li>
+            
+            <li className="text-sm uppercase cursor-pointer"><Link href="/about"> About </Link>
+            </li>
+              
+            </ul>
+          </nav>
+        </header>
+     {children}
+     </body>
     </html>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -2,13 +2,13 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <section className="p-5 md:p-8 bg-white">
-      <div className="container mx-auto flex flex-col md:flex-row items-center justify-between">
-        <div className="w-full md:w-1/2 text-left">
+    <section className="p-5 md:p-8 bg-white ">
+      <div className="container mx-auto  max-w-6xl  flex flex-col md:flex-row items-center justify-between">
+        <div className="w-full md:w-2/5 text-left">
           <h1 className="text-4xl md:text-5xl text-black font-bold">
             Discover what's possible with 3D printing
           </h1>
-          <p className="text-lg text-gray-600 mt-5">
+          <p className="text-s text-xl text-gray-600 mt-5">
             Join our community of creators and explore a vest liberty of
             user-submitted models.
           </p>
@@ -17,12 +17,12 @@ export default function Home() {
           </button>
         </div>
 
-        <div className="w-full md:w-1/2 mt-10 md:mt-0">
+        <div className="w-full md:w-3/5 mt-10 md:mt-0">
           <Image
             src="/frame 4.png"
             alt="3D Printing Hero Image"
-            width={700}
-            height={500}
+            width={470}
+            height={470}
             className="mx-auto mt-10 rounded-lg "
           />
         </div>
