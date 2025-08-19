7d67e7c1ff1bc4f83332f76f98a205a7c5a32386
chinenye-joy <anyimjoy01@gmail.com>
2025-08-19 04:48:33 +0100

    Refactor code structure for improved readability and maintainability

--- app/page.tsx
+++ app/page.tsx
@@ -2,24 +2,30 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <section className=" flex flex-col md:flex-row items-center justify-between p-10 bg-gray-100">
-      <div className="container mx-auto">
-        <h1 className="text-4xl md:text-6xl text-black font-bold text-center md:text-left">Discover what's possible with 3D printing</h1>
-        <p className="text-lg text-gray-700 mt-5">
-          Join our community of creators and explore a vest liberty of
-          use-submitted models
-        </p>
-        <button className=" border-2 border-black bg-white text-black py-2 mt-5 px-3 font-bold">Explore Models</button>
-      </div>
+    <section className="p-5 md:p-8 bg-white">
+      <div className="container mx-auto flex flex-col md:flex-row items-center justify-between">
+        <div className="w-full md:w-1/2 text-left">
+          <h1 className="text-4xl md:text-5xl text-black font-bold">
+            Discover what's possible with 3D printing
+          </h1>
+          <p className="text-lg text-gray-600 mt-5">
+            Join our community of creators and explore a vest liberty of
+            user-submitted models.
+          </p>
+          <button className=" border-2 border-black bg-white text-black hover:bg-gray-950 hover:text-white py-2 mt-5 px-3 transition duration-300 font-bold">
+            Browse Models
+          </button>
+        </div>
 
-      <div className="">
-        <Image
-          src="/frame 4.png"
-          alt="3D Printing Hero Image"
-          width={800}
-          height={600}
-          className="mx-auto mt-10 rounded-lg "
-        />
+        <div className="w-full md:w-1/2 mt-10 md:mt-0">
+          <Image
+            src="/frame 4.png"
+            alt="3D Printing Hero Image"
+            width={700}
+            height={500}
+            className="mx-auto mt-10 rounded-lg "
+          />
+        </div>
       </div>
     </section>
   );
